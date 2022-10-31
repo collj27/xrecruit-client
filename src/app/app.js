0419d2ec14280d824afcb2f78134002a786486ae
James Coll <collj@bu.edu>
2022-10-31 12:31:14 -0400

    configure with back4app

--- src/app/app.js
+++ src/app/app.js
@@ -2,22 +2,32 @@ import './app.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route
-} from 'react-router-dom';
+import Parse from 'parse/dist/parse.min.js';
 import {Component} from "react";
 import Schools from "./schools/schools";
 import PlayerPage from "./playerPage/playerPage";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 import PlayerContainer from "./playerContainer/playerContainer";
+import {PersonComponent} from "./PersonComponent";
+
+const PARSE_APPLICATION_ID = '40QKLduDVPUFjwjyVKfmfnYEMpF9RxnHbun4xNM2';
+const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
+const PARSE_JAVASCRIPT_KEY = 'Bdg3rp5ooY677kNSwPbILQUo34Vj4VmLrLxs5S3h';
+Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = PARSE_HOST_URL;
 
 
 class App extends Component {
     render() {
         return (
-            <Router>
+            <div className="App">
+                <header className="App-header">
+                    <PersonComponent />
+                </header>
+            </div>
+        );
+        {/*<Router>
                 <Navbar expand="md" variant="dark">
                     <Container>
                         <Navbar.Brand href="/"><span>XRECRUIT</span></Navbar.Brand>
@@ -33,45 +43,15 @@ class App extends Component {
                     </Container>
                 </Navbar>
                 <Routes>
-                    {/*<Route exact path='/' element={<PlayerContainer/>}></Route>*/}
                     <Route exact path='/' element={<PlayerPage/>}></Route>
                     <Route exact path='/schools' element={<Schools/>}></Route>
                 </Routes>
 
-            </Router>
-        );
+            </Router>*/}
+
     }
 }
 
-/*<Navbar bg="light" expand="lg">
-    <Container>
-        <Navbar.Brand href="#home">Brand Name/Logo</Navbar.Brand>
-        <Navbar.Collapse>
-            <Nav>
-                <Nav.Link href="#home">Players</Nav.Link>
-                <Nav.Link href="#link">Schools</Nav.Link>
-            </Nav>
-        </Navbar.Collapse>
-    </Container>
-</Navbar>*/
 
-
-/*<Container>
-    <Row>
-        <header class="title">
-            <h1>Top Players</h1>
-        </header>
-    </Row>
-    <Row>
-        {Array.from({length: 4}).map((_, idx) => (
-            <Col>
-                <PlayerCard></PlayerCard>
-            </Col>
-        ))}
-    </Row>
-</Container>
- */
-
-//}
 
 export default App;
