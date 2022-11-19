1454aaf483a7fded390c65d1f56eb8d21811a11d
James Coll <collj@bu.edu>
2022-11-19 18:37:06 -0500

    integrate with flask api and remove parse stuff

--- src/index.jsx
+++ src/index.jsx
@@ -4,16 +4,6 @@ import './index.css';
 import App from './components/app';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// Import Parse minified version
-import Parse from 'parse/dist/parse.min.js';
-
-// Your Parse initialization configuration goes here
-const PARSE_APPLICATION_ID = 'Hcu0HuBLfJoXCzdDz02Ajlq8j3jm25cKsIVcJIDf';
-const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
-const PARSE_JAVASCRIPT_KEY = 'CgSlBlsq2UH52BtPas7m7MC0wm9Fzw08cJCHlrnY';
-Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
-Parse.serverURL = PARSE_HOST_URL;
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/services/playerService.js
+++ src/services/playerService.js
@@ -1,43 +1,9 @@
-import Parse from 'parse/dist/parse.min.js';
-
-
 export async function fetchPlayerById(playerId) {
-    let route = process.env.REACT_APP_API_URL + "players/" + playerId.toString()
-    console.log(route)
-    let playerPromise = fetch(route).then((res) => res.json())
-
-    return playerPromise;
-
-}
-
-export async function fetchPlayerByObjectId(objectId) {
     try {
-        const playerQuery = new Parse.Query('Player')
-        playerQuery.equalTo('objectId', objectId);
-        const player = await playerQuery.first();
-
-        return player
-    } catch (e) {
-        console.log("error", e)
+        let route = process.env.REACT_APP_API_URL + "players/" + playerId.toString()
+        return fetch(route).then((res) => res.json())
+    } catch (e){
+        console.log(e)
         alert("Error fetching player, check console")
-    }
-}
-
-export async function fetchStatsByPlayerId(player) {
-    try {
-       /* const playerQuery = new Parse.Query('Player')
-        playerQuery.equalTo('objectId', playerId);
-        const player = await playerQuery.first();*/
-
-        const tableName = player.get('statsTableName')
-
-        const statsQuery = new Parse.Query(tableName)
-        statsQuery.equalTo('playerId', player)
-        const stats = await statsQuery.find()
-
-        return stats
-    } catch (e) {
-        console.log("error", e)
-        alert("Error fetching stats, check console")
     }
 }
