258be10b86a9b1189389dc5c3e20b0569c3b827b
James Coll <collj@bu.edu>
2022-11-23 11:51:10 -0500

    update stats table

--- src/components/playerProfile/playerStats/playerStats.jsx
+++ src/components/playerProfile/playerStats/playerStats.jsx
@@ -1,6 +1,6 @@
 import Table from "react-bootstrap/Table";
 import {statsColsDict} from "../../../utils/utils";
-
+import './playerStats.css'
 
 //TODO: see if there's a library for this
 function PlayerStats(props) {
@@ -11,67 +11,79 @@ function PlayerStats(props) {
     // only create table when stats is initialized
     if (stats) {
         let keys = []
-        let row = []
 
-        console.log("hii")
+    // TODO: figure out better key than math.random
         // loop through each season's stats
         Object.values(stats).forEach(seasonStatsObj => {
             let objKeys = Object.keys(seasonStatsObj)
             let objArr = Object.entries(seasonStatsObj)
+            let row = []
 
+            if (objKeys.includes("season")) {
+                // if key does not exist in array, add it
+                if (!keys.includes(statsColsDict.season))
+                    keys.push(statsColsDict.season)
 
-            if (objKeys.includes("season") && !keys.includes(statsColsDict.season)) {
-                keys.push(statsColsDict.season)
+                // get data cell value and add to row array
                 let keyValue = objArr.find(x => x[0] === "season")
                 let value = keyValue[1]
-                row.push(<td>{value}</td>)
+                row.push(<td key={Math.random()}>{value}</td>)
             }
 
-            if (objKeys.includes("passing_tds") && !keys.includes(statsColsDict.passing_tds)) {
-                keys.push(statsColsDict.passing_tds)
+            if (objKeys.includes("passing_tds")) {
+                if (!keys.includes(statsColsDict.passing_tds))
+                    keys.push(statsColsDict.passing_tds)
                 let keyValue = objArr.find(x => x[0] === "passing_tds")
                 let value = keyValue[1]
-                row.push(<td>{value}</td>)
+                row.push(<td key={Math.random()}>{value}</td>)
             }
 
-            if (objKeys.includes("passing_yds") && !keys.includes(statsColsDict.passing_yds)) {
-                keys.push(statsColsDict.passing_yds)
-                let value = objArr.find(x => x[0] === "passing_yds")
-               // body.push(<tr><td>{value}</td>)}</tr>)
+            if (objKeys.includes("passing_yds")) {
+                if (!keys.includes(statsColsDict.passing_yds))
+                    keys.push(statsColsDict.passing_yds)
+                let keyValue = objArr.find(x => x[0] === "passing_yds")
+                let value = keyValue[1]
+                row.push(<td key={Math.random()}>{value}</td>)
             }
 
-            if (objKeys.includes("rushing_tds") && !keys.includes(statsColsDict.rushing_tds)) {
-                keys.push(statsColsDict.rushing_tds)
-                let value = objArr.find(x => x[0] === "rushing_tds")
-                //body.push(<tr><td>{value}</td>)}</tr>)
+            if (objKeys.includes("rushing_tds")) {
+                if (!keys.includes(statsColsDict.rushing_tds))
+                    keys.push(statsColsDict.rushing_tds)
+                let keyValue = objArr.find(x => x[0] === "rushing_tds")
+                let value = keyValue[1]
+                row.push(<td key={Math.random()}>{value}</td>)
             }
 
 
-            if (objKeys.includes("rushing_yds") && !keys.includes(statsColsDict.rushing_yds)) {
-                keys.push(statsColsDict.rushing_yds)
-                let value = objArr.find(x => x[0] === "rushing_yds")
-                //body.push(<tr><td>{value}</td>)}</tr>)
+            if (objKeys.includes("rushing_yds")) {
+                if (!keys.includes(statsColsDict.rushing_yds))
+                    keys.push(statsColsDict.rushing_yds)
+                let keyValue = objArr.find(x => x[0] === "rushing_yds")
+                let value = keyValue[1]
+                row.push(<td key={Math.random()}>{value}</td>)
             }
 
-            if (objKeys.includes("interceptions") && !keys.includes(statsColsDict.interceptions)) {
-                keys.push(statsColsDict.interceptions)
-                let value = objArr.find(x => x[0] === "interceptions")
-               // body.push(<tr><td>{value}</td>)}</tr>)
+            if (objKeys.includes("interceptions")) {
+                if (!keys.includes(statsColsDict.interceptions))
+                    keys.push(statsColsDict.interceptions)
+                let keyValue = objArr.find(x => x[0] === "interceptions")
+                let value = keyValue[1]
+                row.push(<td key={Math.random()}>{value}</td>)
             }
 
-            if (objKeys.includes("fumbles")&& !keys.includes(statsColsDict.fumbles)) {
-                keys.push(statsColsDict.fumbles)
-                let value = objArr.find(x => x[0] === "fumbles")
-                //body.push(<tr><td>{value}</td>)}</tr>)
+            if (objKeys.includes("fumbles")) {
+                if (!keys.includes(statsColsDict.fumbles))
+                    keys.push(statsColsDict.fumbles)
+                let keyValue = objArr.find(x => x[0] === "fumbles")
+                let value = keyValue[1]
+                row.push(<td key={Math.random()}>{value}</td>)
             }
 
-            console.log(row)
-            body.push(<tr>{row}</tr>)
+            body.push(<tr key={Math.random()}>{row}</tr>)
 
-        });
+        })
 
         // create header element from keys and stats cols dictionary
-
         header = keys.map((k, index) => <th scope='col' key={index}>{k}</th>)
     }
 
