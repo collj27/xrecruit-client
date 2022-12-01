2d82ffc98f63f3000c430b71d24d57b0ad2c24e7
James Coll <collj@bu.edu>
2022-12-01 14:54:43 -0500

    remove position enum and fix datasetting

--- src/components/playerProfile/playerProfile.jsx
+++ src/components/playerProfile/playerProfile.jsx
@@ -10,7 +10,7 @@ import {Button} from "react-bootstrap";
 import '../app.css'
 import {ArrowRight} from "react-bootstrap-icons";
 import {fetchPlayerById} from "../../services/playerService";
-import {calculateAge, positionDict} from "../../utils/utils";
+import {calculateAge} from "../../utils/utils";
 
 // auth
 
@@ -21,17 +21,18 @@ function PlayerProfile() {
     useEffect(() => {
 
         fetchPlayerById(1).then((data) => {
+            console.log(data)
             setPlayer({
-                    firstName: data.first_name,
-                    lastName: data.last_name,
-                    position: positionDict[data.position],
-                    height: data.height,
-                    weight: data.weight,
-                    age: calculateAge(data.birth_date),
-                    highSchool: data.high_school,
-                    description: data.description,
-                    stats: data.player_stats,
-                    imgUrl: data.player_img_url,
+                    firstName: data["first_name"],
+                    lastName: data["last_name"],
+                    position: data["position"],
+                    height: data["height"],
+                    weight: data["weight"],
+                    age: calculateAge(data["birth_date"]),
+                    highSchool: data["high_school"],
+                    description: data["description"],
+                    stats: data["player_stats"],
+                    imgUrl: data["image_url"],
                     videoUrl: "https://www.youtube.com/watch?v=JWVQF5_gkfk"
                 }
             )
--- src/components/schoolProfile/schoolProfile.jsx
+++ src/components/schoolProfile/schoolProfile.jsx
@@ -2,31 +2,25 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
-import ReactPlayer from "react-player";
 import {useEffect, useState} from "react";
-import {Button, ListGroup} from "react-bootstrap";
+import {ListGroup} from "react-bootstrap";
 import '../app.css'
-import {ArrowRight} from "react-bootstrap-icons";
-import {fetchPlayerById} from "../../services/playerService";
-import {calculateAge, positionDict} from "../../utils/utils";
 import {fetchSchoolById} from "../../services/schoolService";
+import ReactPlayer from "react-player";
 
-// auth
 
-//TODO: create stats table
 function SchoolProfile() {
     const [school, setSchool] = useState(null);
 
     useEffect(() => {
-
         fetchSchoolById(1).then((data) => {
-
             console.log(data)
+
             setSchool({
                     name: data.name,
-                    newsArticles: data.news,
-                    imgUrl: data.school_img_url,
-                    videoUrl: "https://www.youtube.com/watch?v=JWVQF5_gkfk"
+                    newsArticles: data["news_articles"],
+                    imgUrl: data["image_url"],
+                    videoUrl: "https://www.youtube.com/watch?v=2P6kKQPbsRE"
                 }
             )
         })
@@ -34,34 +28,44 @@ function SchoolProfile() {
     }, []);
     return (
         <Container fluid="md">
-            <Row className="justify-content-center">
-                <Col xl={3} className="mt-5">
-                    <Card>
-                        {/*TODO: figure out proper image size}*/}
-                        <Card.Img variant="top" src={school?.imgUrl}/>
-                        <Card.Body>
-                            <Card.Title>
-                                <span>{school?.name}</span>
-                            </Card.Title>
-                        </Card.Body>
-                    </Card>
+            <Row className="justify-content-center mt-5">
+                {/*left half of page*/}
+                <Col>
+                    <Row>
+                        <Col>
+                            <Card>
+                                {/*TODO: figure out proper image size}*/}
+                                <Card.Img variant="top" src={school?.imgUrl}/>
+                                <Card.Body>
+                                    <Card.Title>
+                                        <span>{school?.name}</span>
+                                    </Card.Title>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="mt-5">
+                            <Card>
+                                <Card.Header>Featured</Card.Header>
+                                <ListGroup variant="flush">
+                                    {school?.newsArticles.map((x, index) =>
+                                        <ListGroup.Item>
+                                            <Card.Link key={index} href={x.formattedUrl}>{x.title}</Card.Link>
+                                        </ListGroup.Item>
+                                    )}
+                                </ListGroup>
+                            </Card>
+                        </Col>
+                    </Row>
                 </Col>
-            </Row>
-            <Row className="justify-content-center">
-                <Col xl={3} className="mt-5">
-                    <Card>
-                        <Card.Header>Featured</Card.Header>
-                        <ListGroup variant="flush">
-                            {school?.newsArticles.map((x, index) =>
-                                <ListGroup.Item>
-                                    <Card.Link key={index} href={x.formattedUrl}>{x.title}</Card.Link>
-                                </ListGroup.Item>
 
-                            )}
-                        </ListGroup>
-                    </Card>
+                {/*right half of page*/}
+                <Col>
+                    <ReactPlayer url={school?.videoUrl}/>
                 </Col>
             </Row>
+
         </Container>
     );
 }
