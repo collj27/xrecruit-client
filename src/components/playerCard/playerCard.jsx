dab39879a3b5dfebdf20931399807fc26f121656
James Coll <collj@bu.edu>
2023-01-24 15:46:15 -0500

    scale image to fit container and preserve aspect ratio

--- src/components/playerCard/playerCard.jsx
+++ src/components/playerCard/playerCard.jsx
@@ -1,16 +1,17 @@
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import {Stack} from "react-bootstrap";
+import {Button, Stack} from "react-bootstrap";
 import "./playerCard.css"
+import {Plus, PlusLg} from "react-bootstrap-icons";
 
 function PlayerCard(props) {
     return (
         <Card className="playerCard">
             <Card.Body>
                 <Row>
-                    <Col>
-                        <Card.Img src={props.imgUrl} height="173px"/>
+                    <Col className="playerCard-image-container">
+                        <Card.Img className="playerCard-image" src={props.imgUrl}/>
                     </Col>
                     <Col sm={4} className="text-end">
                         <Stack>
@@ -20,11 +21,24 @@ function PlayerCard(props) {
                     </Col>
                 </Row>
                 <Row>
-                    <Stack>
-                        <span className="playerCard-name">{props.firstName + " " + props.lastName}</span>
-                        <span className="playerCard-funds">$350k Raised</span>
-                        <span className="playerCard-funds">203 Donors</span>
-                    </Stack>
+                    <Col>
+                        <Stack>
+                            <span className="playerCard-name">{props.firstName + " " + props.lastName}</span>
+                            <span className="playerCard-funds">$350k Raised</span>
+                            <Row className="align-items-center">
+                                <Col>
+                                    <span className="playerCard-funds">203 Donors</span>
+                                </Col>
+                                <Col className="text-end">
+                                    <Button className="btn btn-secondary"><PlusLg></PlusLg></Button>
+                                </Col>
+                            </Row>
+
+
+                        </Stack>
+                    </Col>
+
+
                 </Row>
             </Card.Body>
         </Card>
