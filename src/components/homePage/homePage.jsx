d3c202c1d0bbe6e5b3062b8b54c2c4b627eab471
James Coll <collj@bu.edu>
2023-01-24 15:02:00 -0500

    update styling

--- src/components/homePage/homePage.jsx
+++ src/components/homePage/homePage.jsx
@@ -1,4 +1,4 @@
-import {Form} from "react-bootstrap";
+import {Card, Form} from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
@@ -9,11 +9,8 @@ import {fetchPlayers} from "../../services/playerService";
 
 function HomePage() {
 
-    //TODO: make this a list
-    //player data
     const [playerList, setPlayerList] = useState(null);
     useEffect(() => {
-
         fetchPlayers().then((data) => {
             setPlayerList(data)
         })
@@ -24,7 +21,7 @@ function HomePage() {
         <Container>
             <Row className="mt-5">
                 <Col>
-                    <h1>Help bring the best players to your favorite team.</h1>
+                    <h1 className="homePage-title">Help bring the best players to your favorite team.</h1>
                 </Col>
             </Row>
             <Row className="mt-3">
@@ -38,26 +35,30 @@ function HomePage() {
                     </Form>
                 </Col>
             </Row>
+            <Row className="my-3">
+                <Card>
+                    <Card.Body>
+                        <Row xs={2} sm={2} md={3} lg={4} className="my-3">
+                            {playerList?.map((player, idx) => (
+                                <Col key={idx}>
+                                    <PlayerCard firstName={player["first_name"]} lastName={player["last_name"]}
+                                                imgUrl={player["image_url"]} position={player["position"]}></PlayerCard>
+                                </Col>
+                            ))}
+                        </Row>
 
+                        <Row md={4} className="my-3">
+                            {playerList?.map((player, idx) => (
+                                <Col key={idx}>
+                                    <PlayerCard firstName={player["first_name"]} lastName={player["last_name"]}
+                                                imgUrl={player["image_url"]} position={player["position"]}></PlayerCard>
+                                </Col>
+                            ))}
+                        </Row>
 
-            <Row xs={2} sm={2} md={3} lg={4} className="my-3">
-                {playerList?.map((player, idx) => (
-                    <Col key={idx}>
-                        <PlayerCard firstName={player["first_name"]} lastName={player["last_name"]}
-                                    imgUrl={player["image_url"]} position={player["position"]}></PlayerCard>
-                    </Col>
-                ))}
+                    </Card.Body>
+                </Card>
             </Row>
-
-            <Row md={4} className="my-3">
-                {playerList?.map((player, idx) => (
-                    <Col key={idx}>
-                        <PlayerCard firstName={player["first_name"]} lastName={player["last_name"]}
-                                    imgUrl={player["image_url"]} position={player["position"]}></PlayerCard>
-                    </Col>
-                ))}
-            </Row>
-
         </Container>
 
     );
--- src/components/playerCard/playerCard.jsx
+++ src/components/playerCard/playerCard.jsx
@@ -6,30 +6,28 @@ import "./playerCard.css"
 
 function PlayerCard(props) {
     return (
-
-                <Card>
-                    <Card.Body>
-                        <Row>
-                            <Col>
-                                <Card.Img src={props.imgUrl} height="173px"/>
-                            </Col>
-                            <Col sm={4} className="text-end">
-                                <Stack>
-                                    <span className="playerCard-number-position">#8</span>
-                                    <span className="playerCard-number-position">{props.position}</span>
-                                </Stack>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Stack>
-                                <span className="playerCard-name">{props.firstName + " " + props.lastName}</span>
-                                <span className="playerCard-funds">$350k Raised</span>
-                                <span className="playerCard-funds">203 Donors</span>
-                            </Stack>
-                        </Row>
-                    </Card.Body>
-                </Card>
-
+        <Card className="playerCard">
+            <Card.Body>
+                <Row>
+                    <Col>
+                        <Card.Img src={props.imgUrl} height="173px"/>
+                    </Col>
+                    <Col sm={4} className="text-end">
+                        <Stack>
+                            <span className="playerCard-number-position">#8</span>
+                            <span className="playerCard-number-position">{props.position}</span>
+                        </Stack>
+                    </Col>
+                </Row>
+                <Row>
+                    <Stack>
+                        <span className="playerCard-name">{props.firstName + " " + props.lastName}</span>
+                        <span className="playerCard-funds">$350k Raised</span>
+                        <span className="playerCard-funds">203 Donors</span>
+                    </Stack>
+                </Row>
+            </Card.Body>
+        </Card>
 
 
     );
